a1fe7e2f1af6e2cfaedb34eff21a050f1ae3045f
Peter <peterabsolon@yahoo.com>
2020-12-04 22:12:32 +0100

    Basics

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {
+    semi: 0,
+    'arrow-body-style': 0,
+
+    'import/prefer-default-export': 0,
+    'import/extensions': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { main } from './index'
+
+describe('main', () => {
+  it('works', () => {
+    expect(main()).toBe(2)
+  })
+})
