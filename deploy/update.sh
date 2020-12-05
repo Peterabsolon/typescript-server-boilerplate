1a46d2a76d8092416c2e63a0206f223d10eb0f9e
Peter <peterabsolon@yahoo.com>
2020-12-05 15:07:00 +0100

    Wut

--- deploy/update.sh
+++ deploy/update.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-env
-
 python /usr/local/service_account_ssh.py \
-    --cmd "cd /usr/local && docker-compose down && DRONE_BUILD_NUMBER=${DRONE_BUILD_NUMBER} docker-compose up -d" \
+    --cmd "echo $DRONE_BUILD_NUMBER && cd /usr/local && docker-compose down && DRONE_BUILD_NUMBER=${DRONE_BUILD_NUMBER} docker-compose up -d" \
     --account ssh-master-account@mvp-api-254102.iam.gserviceaccount.com \
     --project mvp-api-254102 --hostname $HOSTNAME
