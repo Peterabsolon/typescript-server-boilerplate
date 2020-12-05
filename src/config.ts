8394ffb9c614f5a709b191f5b88d1e9f5b372658
Peter <peterabsolon@yahoo.com>
2020-12-05 14:57:19 +0100

    Fix pg version

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const config = {
   port: process.env.PORT || 8080,
 
   pg: {
-    version: '13.0',
+    version: '13.1',
     host: process.env.PGHOST || 'localhost',
     database: process.env.PGDATABASE || 'postgres',
     port: Number(process.env.PGPORT) || 5433,
--- src/db/index.test.ts
+++ src/db/index.test.ts
@@ -1,8 +1,10 @@
 import { db } from '.'
 
+import { config } from '../config'
+
 describe('ping', () => {
   it('can ping db', async () => {
     const res = await db.ping()
-    expect(res.version.slice(0, 15)).toBe('PostgreSQL 13.0')
+    expect(res.version.slice(0, 15)).toBe(`PostgreSQL ${config.pg.version}`)
   })
 })
