ececaee39ceda850df9ee331da39a9b812ff3a33
Peter <peterabsolon@yahoo.com>
2020-12-05 15:01:37 +0100

    Fix deploy?

--- deploy/update.sh
+++ deploy/update.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+env
+
 python /usr/local/service_account_ssh.py \
     --cmd "cd /usr/local && docker-compose down && DRONE_BUILD_NUMBER=${DRONE_BUILD_NUMBER} docker-compose up -d" \
     --account ssh-master-account@mvp-api-254102.iam.gserviceaccount.com \
