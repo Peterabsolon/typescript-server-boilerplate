f48db6d57d3e5d3923430af51cfefc6b67af51c6
Peter <peterabsolon@yahoo.com>
2020-12-05 02:56:23 +0100

    Try Drone

---
+++ deploy/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+python /usr/local/service_account_ssh.py \
+    --cmd "cd /usr/local && docker-compose down && DRONE_BUILD_NUMBER=${DRONE_BUILD_NUMBER} docker-compose up -d" \
+    --account ssh-master-account@mvp-api-254102.iam.gserviceaccount.com \
+    --project mvp-api-254102 --hostname $HOSTNAME
